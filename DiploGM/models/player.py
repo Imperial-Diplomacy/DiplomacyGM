a652c8abf81013b094909c9482f19aad57a3059a
Harry White <harry@hdwhite.org>
2026-03-27 19:06:39 -0400

    Added NPC players and simple SVG validation

--- DiploGM/models/player.py
+++ DiploGM/models/player.py
@@ -38,6 +38,7 @@ class Player:
         color: str | dict[str, str],
         centers: set[province.Province],
         units: set[unit.Unit],
+        is_active: bool = True
     ):
         self.name: str = name
         self.color_dict: dict | None = None
@@ -62,6 +63,8 @@ class Player:
         self.points: int = 0
         self.liege: Player | None = None
         self.vassals: list[Player] = []
+
+        self.is_active: bool = is_active
 
         # Must be initialised when the board is made
         self.board: Optional[Board] = None
@@ -146,6 +149,6 @@ class Player:
         scs = len(self.centers)
         if scs >= 6:
             return PlayerClass.EMPIRE
-        elif scs >= 3:
+        if scs >= 3:
             return PlayerClass.KINGDOM
         return PlayerClass.DUCHY
